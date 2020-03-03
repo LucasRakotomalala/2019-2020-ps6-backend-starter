47dd1d1e9166f4b5913ea84e3d51cce6551eebd3
Lucas RAKOTOMALALA <lucas.rakotomalala@yahoo.fr>
2020-03-03 10:46:00 +0100

    all

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,8 +1,8 @@
-/*const originalConsoleLog = console.log
+/* const originalConsoleLog = console.log
 console.log = function consoleLog(...args) {
   const newArguments = [`[${new Date().toISOString()}]`, ...args]
   return originalConsoleLog.apply(this, newArguments)
 }
 console.info = console.log
 
-module.exports = console*/
+module.exports = console */
